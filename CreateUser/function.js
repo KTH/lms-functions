1d41dd2c15ff9e40869a8ce34c8ae01403b0d188
Elena Rakhimova <elenara@kth.se>
2018-07-03 14:58:02 +0200

    Added connection to azure bus topic

---
+++ CreateUser/function.js
@@ -0,0 +1,16 @@
+{
+    {
+        "bindings": [
+          {
+            "name": "mySbMsg",
+            "type": "serviceBusTrigger",
+            "direction": "in",
+            "topicName": "ug-infoclass-2",
+            "subscriptionName": "canvas-ref",
+            "connection": "kth-integral_RootManageSharedAccessKey_SERVICEBUS",
+            "accessRights": "Listen"
+          }
+        ],
+        "disabled": false
+      }
+}
