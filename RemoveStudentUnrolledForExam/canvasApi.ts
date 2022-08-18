42f576c5cedb206ef79b297ad06cae58130c24e5
Emil Stenberg <esandin@gmail.com>
2022-08-18 10:20:50 +0200

    use Type:Enrollment

--- RemoveStudentUnrolledForExam/canvasApi.ts
+++ RemoveStudentUnrolledForExam/canvasApi.ts
@@ -5,41 +5,47 @@ let canvasApi: CanvasApi;
 
 if (process.env.CANVAS_API_URL) {
   canvasApi = new CanvasApi(
-      process.env.CANVAS_API_URL,
-      process.env.CANVAS_API_ADMIN_TOKEN
+    process.env.CANVAS_API_URL,
+    process.env.CANVAS_API_ADMIN_TOKEN
   );
 } else {
   throw(new Error("Missing env-var CANVAS_API_URL"));
 }
 
+type Enrollment = {id:number}
+
 /**
  * Get student course enrollment from Canvas.
  */
 /* export async function getCourseEnrollments(activityRoundId: string, studentId: string) { */
-export async function getCourseEnrollments(activityRoundId: string, studentId: string) : Promise<[{id: number}]>{
+export async function getCourseEnrollments(activityRoundId: string, studentId: string) : Promise<[Enrollment]>{
   assert(canvasApi, "Missing canvasApi");
   // https://canvas.instructure.com/doc/api/enrollments.html#method.enrollments_api.index
   // kthId is called sis_user_id in KTH Canvas
   // We need id of enrollment for the current use case so I passing it to the generic
-  let getEnrollments = async (sectionId: string): Promise<[{id:number}]>=>{
+  let getEnrollments = async (sectionId: string): Promise<[Enrollment]>=>{
 
     console.log(`sections/sis_section_id:${sectionId}/enrollments`)
-    let res = await canvasApi.get<[{id:number}]>(
+    let res = await canvasApi.get<[Enrollment]>(
 
-    `sections/sis_section_id:${sectionId}/enrollments`,
-    {
-      "user_id": `sis_integration_id:${studentId}`,
-      "type": ["StudentEnrollment"],
-    }
-  )
-  return res.body;
+      `sections/sis_section_id:${sectionId}/enrollments`,
+      {
+        "user_id": `sis_integration_id:${studentId}`,
+        "type": ["StudentEnrollment"],
+      }
+    )
+    return res.body;
   }
 
   // TODO: Handle errors better
 
-  /* return [ */
-    return getEnrollments(`AKT.${activityRoundId}`)//, 
-    /* await getEnrollment(`AKT.${activityRoundId}.FUNKA`)] */ 
+  return [
+    ... await getEnrollments(`AKT.${activityRoundId}`),
+    /* ... await getEnrollments(`AKT.${activityRoundId}.FUNKA`) */
+
+  ]
+  //, 
+  /* await getEnrollment(`AKT.${activityRoundId}.FUNKA`)] */ 
 }
 
 /**
@@ -48,14 +54,14 @@ export async function getCourseEnrollments(activityRoundId: string, studentId: s
 export async function removeEnrollment(courseId: string, studentId: string) : Promise<void> {
   assert(canvasApi, "Missing canvasApi");
 
-const enrollments = await getCourseEnrollments(courseId, studentId)
-console.log(enrollments)
+  const enrollments = await getCourseEnrollments(courseId, studentId)
+  console.log(enrollments)
 
-/* const enrollments1 = await canvasApi.listItems(`courses/sis_course_id:${courseId}/enrollments`).toArray(); */
-/* const enrollments2 = await canvasApi.listItems(`courses/sis_course_id:${courseId}/enrollments`).toArray(); */
-/* const usersEnrollments = [...enrollments1, enrollments2].filter(enrollment => enrollment.user?.integration_id ===studentId) */
-/* console.log(usersEnrollments) */
-// Now `courses` is an iterator that goes through every course
+  /* const enrollments1 = await canvasApi.listItems(`courses/sis_course_id:${courseId}/enrollments`).toArray(); */
+  /* const enrollments2 = await canvasApi.listItems(`courses/sis_course_id:${courseId}/enrollments`).toArray(); */
+  /* const usersEnrollments = [...enrollments1, enrollments2].filter(enrollment => enrollment.user?.integration_id ===studentId) */
+  /* console.log(usersEnrollments) */
+  // Now `courses` is an iterator that goes through every course
   /* const res = await canvasApi.request( */
   /*   `courses/${courseId}/enrollments/${studentId}?task=delete`, */
   /*   "DELETE", */
