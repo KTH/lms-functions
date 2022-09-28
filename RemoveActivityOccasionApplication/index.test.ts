29a42a43b50516c5220f01c5b9c0ea69ddfa9b4f
Emil Stenberg <esandin@gmail.com>
2022-09-28 15:08:17 +0200

    removed unused data from tests

--- RemoveActivityOccasionApplication/index.test.ts
+++ RemoveActivityOccasionApplication/index.test.ts
@@ -13,91 +13,13 @@ test('An AddRegistration message should NOT be considered a removeActivity',()=>
       <ns0:member>
          <ns0:personSourcedId>d7a5ac5e-9627-11e8-bf01-45625eef45e2</ns0:personSourcedId>
          <ns0:role>
-            <ns0:roleType>Learner</ns0:roleType>
-            <ns0:timeFrame>
-               <ns0:begin />
-               <ns0:end />
-            </ns0:timeFrame>
-            <ns0:status>Active</ns0:status>
             <ns0:extension>
-               <ns0:extensionNameVocabulary>LocallyDefined</ns0:extensionNameVocabulary>
-               <ns0:extensionTypeVocabulary>http://www.imsglobal.org/lis/cmsv1p0/extensionvocabularyv1p0</ns0:extensionTypeVocabulary>
-               <ns0:extensionField>
-                  <ns0:fieldName>Admitted</ns0:fieldName>
-                  <ns0:fieldType>Boolean</ns0:fieldType>
-                  <ns0:fieldValue>true</ns0:fieldValue>
-               </ns0:extensionField>
-               <ns0:extensionField>
-                  <ns0:fieldName>Registered</ns0:fieldName>
-                  <ns0:fieldType>Boolean</ns0:fieldType>
-                  <ns0:fieldValue>true</ns0:fieldValue>
-               </ns0:extensionField>
-               <ns0:extensionField>
-                  <ns0:fieldName>Break</ns0:fieldName>
-                  <ns0:fieldType>Boolean</ns0:fieldType>
-                  <ns0:fieldValue>false</ns0:fieldValue>
-               </ns0:extensionField>
-               <ns0:extensionField>
-                  <ns0:fieldName>Dropout</ns0:fieldName>
-                  <ns0:fieldType>Boolean</ns0:fieldType>
-                  <ns0:fieldValue>false</ns0:fieldValue>
-               </ns0:extensionField>
                <ns0:extensionField>
                   <ns0:fieldName>OriginEvent</ns0:fieldName>
                   <ns0:fieldType>string</ns0:fieldType>
                   <ns0:fieldValue>LADOK.AddRegistration</ns0:fieldValue>
                </ns0:extensionField>
-               <ns0:extensionField>
-                  <ns0:fieldName>EarlyAccess</ns0:fieldName>
-                  <ns0:fieldType>Boolean</ns0:fieldType>
-                  <ns0:fieldValue>false</ns0:fieldValue>
-               </ns0:extensionField>
-               <ns0:extensionField>
-                  <ns0:fieldName>participation.program.code</ns0:fieldName>
-                  <ns0:fieldType>string</ns0:fieldType>
-                  <ns0:fieldValue>TIBYH</ns0:fieldValue>
-               </ns0:extensionField>
-               <ns0:extensionField>
-                  <ns0:fieldName>participation.programdirection.code</ns0:fieldName>
-                  <ns0:fieldType>string</ns0:fieldType>
-                  <ns0:fieldValue>HUPK</ns0:fieldValue>
-               </ns0:extensionField>
-               <ns0:extensionField>
-                  <ns0:fieldName>education.is.latter.part</ns0:fieldName>
-                  <ns0:fieldType>Boolean</ns0:fieldType>
-                  <ns0:fieldValue>false</ns0:fieldValue>
-               </ns0:extensionField>
-               <ns0:extensionField>
-                  <ns0:fieldName>education.is.root.packaging</ns0:fieldName>
-                  <ns0:fieldType>Boolean</ns0:fieldType>
-                  <ns0:fieldValue>false</ns0:fieldValue>
-               </ns0:extensionField>
-               <ns0:extensionField>
-                  <ns0:fieldName>education.code</ns0:fieldName>
-                  <ns0:fieldType>string</ns0:fieldType>
-                  <ns0:fieldValue>AF179X</ns0:fieldValue>
-               </ns0:extensionField>
-               <ns0:extensionField>
-                  <ns0:fieldName>education.instance.code</ns0:fieldName>
-                  <ns0:fieldType>string</ns0:fieldType>
-                  <ns0:fieldValue>50069</ns0:fieldValue>
-               </ns0:extensionField>
-               <ns0:extensionField>
-                  <ns0:fieldName>education.uid</ns0:fieldName>
-                  <ns0:fieldType>string</ns0:fieldType>
-                  <ns0:fieldValue>2aee98c0-73d8-11e8-afa7-8e408e694e54</ns0:fieldValue>
-               </ns0:extensionField>
-               <ns0:extensionField>
-                  <ns0:fieldName>education.version.uid</ns0:fieldName>
-                  <ns0:fieldType>string</ns0:fieldType>
-                  <ns0:fieldValue>2ae83041-73d8-11e8-b4e0-063f9afb40e3</ns0:fieldValue>
-               </ns0:extensionField>
-               <ns0:extensionField>
-                  <ns0:fieldName>education.type.id</ns0:fieldName>
-                  <ns0:fieldType>string</ns0:fieldType>
-                  <ns0:fieldValue>52</ns0:fieldValue>
-               </ns0:extensionField>
-            </ns0:extension>
+              </ns0:extension>
          </ns0:role>
       </ns0:member>
    </ns0:membership>
@@ -115,9 +37,6 @@ test('A removeActivity message should be handled',()=>{
       <ns0:member>
          <ns0:personSourcedId>7451ed4c-4376-11eb-aa19-cdd070ea4b24</ns0:personSourcedId>
          <ns0:role>
-            <ns0:roleType>Student</ns0:roleType>
-            <ns0:status>Inactive</ns0:status>
-            <ns0:dataSource>Ladok</ns0:dataSource>
             <ns0:extension>
                <ns0:extensionField>
                   <ns0:fieldName>OriginEvent</ns0:fieldName>
