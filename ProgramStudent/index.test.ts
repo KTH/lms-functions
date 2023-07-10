a11fadf48afde62c30a6f6ac73005fad91faecc8
Emil <esandin@gmail.com>
2023-07-10 11:14:12 +0200

    added skeleton for program tests

---
+++ ProgramStudent/index.test.ts
@@ -0,0 +1,5 @@
+describe("programroom registration", () => {
+  test.todo("is triggered by registration message with program code");
+  test.todo("is not triggered by registration message without program code");
+  test.todo("is not triggered by message without registration");
+});
