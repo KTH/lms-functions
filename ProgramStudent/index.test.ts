e06fc3ff6bd780cccf2c799137a22ecfa2322417
Sebastian Ware <sebastian@urbantalk.se>
2023-07-11 10:16:01 +0200

    Added test WIP

--- ProgramStudent/index.test.ts
+++ ProgramStudent/index.test.ts
@@ -1,9 +1,11 @@
 import { Context } from "vm";
-import { registrationMessageWithoutProgramCode } from "../lis.test.messages";
+import {
+  registrationMessageWithProgram,
+  registrationMessageWithoutProgramCode,
+} from "../lis.test.messages";
 import { enrollRegisteredProgramStudentIfApplicable } from "./index";
 
 describe("programroom registration", () => {
-  test.todo("is triggered by registration message with program code");
   test("is not triggered by registration message without program code", async () => {
     expect(
       await enrollRegisteredProgramStudentIfApplicable(
@@ -12,5 +14,13 @@ describe("programroom registration", () => {
       )
     ).toBeFalsy();
   });
+  test("is triggered by registration message with program code", async () => {
+    const result = await enrollRegisteredProgramStudentIfApplicable(
+      { log: () => {} } as Context,
+      registrationMessageWithProgram
+    );
+    expect(result).toBeTruthy();
+  });
   test.todo("is not triggered by message without registration");
+  test.todo("produces proper output when receiving a valid message");
 });
